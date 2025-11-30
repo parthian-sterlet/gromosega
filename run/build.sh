74308c503e98091b99f89e43b7e613270a6733cf
Victor Levitsky <68056117+parthian-sterlet@users.noreply.github.com>
2025-11-30 15:21:08 +0700

    Update build.sh

--- run/build.sh
+++ run/build.sh
@@ -3,9 +3,9 @@
 chmod a+x command_line_chipseq
 chmod a+x command_line_rnaseq
 
-g++ -o minimax.exe ../src/minimax.cpp
-g++ -o table_rnaseq_filter.exe ../src/table_rnaseq_filter.cpp
-g++ -o select_lines01.exe ../src/select_lines01.cpp
+g++ -o minimax.exe minimax.cpp
+g++ -o table_rnaseq_filter.exe table_rnaseq_filter.cpp
+g++ -o select_lines01.exe select_lines01.cpp
 
 chmod a+x minimax.exe
 chmod a+x table_rnaseq_filter.exe
