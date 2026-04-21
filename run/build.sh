f684f0c9f3af817fc5eb9f21746fe8c2aa12239f
Victor Levitsky <68056117+parthian-sterlet@users.noreply.github.com>
2026-04-21 10:01:34 +0700

    Update build.sh

--- run/build.sh
+++ run/build.sh
@@ -9,12 +9,14 @@ cd ../src
 
 g++ -o minimax.exe minimax.cpp
 g++ -o minimax_pars.exe minimax_pars.cpp
+g++ -o minimax_sort.exe minimax_sort.cpp
 g++ -o table_rnaseq_adapted.exe table_rnaseq_adapted.cpp
 g++ -o table_rnaseq_knife.exe table_rnaseq_knife.cpp
 g++ -o select_lines01.exe select_lines01.cpp
 
 chmod a+x minimax.exe
 chmod a+x minimax_pars.exe
+chmod a+x minimax_sort.exe
 chmod a+x table_rnaseq_adapted.exe
 chmod a+x table_rnaseq_knife.exe
 chmod a+x select_lines01.exe
